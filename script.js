e7b95168f8caa049a4dd2ff9ef9c9b88ec52f833
cassandra-mn <arthenciac@gmail.com>
2022-01-28 13:13:02 -0300

    versão com botão de finalizar pedido pronto

--- script.js
+++ script.js
@@ -1,80 +1,102 @@
+let prato = 0;
+let bebida = 0;
+let sobremesa = 0;
+
 function prato1() {
-    const escolha = document.querySelector(".prato1");
-    escolha.classList.toggle("escolhido");
+    prato = document.querySelector(".prato1");
+    prato.classList.add("escolhido");
     const remover2 = document.querySelector(".prato2");
     remover2.classList.remove("escolhido");
     const remover3 = document.querySelector(".prato3");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function prato2() {
-    const escolha = document.querySelector(".prato2");
-    escolha.classList.toggle("escolhido");
+    prato = document.querySelector(".prato2");
+    prato.classList.add("escolhido");
     const remover1 = document.querySelector(".prato1");
     remover1.classList.remove("escolhido");
     const remover3 = document.querySelector(".prato3");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function prato3() {
-    const escolha = document.querySelector(".prato3");
-    escolha.classList.toggle("escolhido");
+    prato = document.querySelector(".prato3");
+    prato.classList.add("escolhido");
     const remover1 = document.querySelector(".prato1");
     remover1.classList.remove("escolhido");
     const remover3 = document.querySelector(".prato2");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function bebida1() {
-    const escolha = document.querySelector(".bebida1");
-    escolha.classList.toggle("escolhido");
+    bebida = document.querySelector(".bebida1");
+    bebida.classList.toggle("escolhido");
     const remover2 = document.querySelector(".bebida2");
     remover2.classList.remove("escolhido");
     const remover3 = document.querySelector(".bebida3");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function bebida2() {
-    const escolha = document.querySelector(".bebida2");
-    escolha.classList.toggle("escolhido");
+    bebida = document.querySelector(".bebida2");
+    bebida.classList.toggle("escolhido");
     const remover1 = document.querySelector(".bebida1");
     remover1.classList.remove("escolhido");
     const remover3 = document.querySelector(".bebida3");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function bebida3() {
-    const escolha = document.querySelector(".bebida3");
-    escolha.classList.toggle("escolhido");
+    bebida = document.querySelector(".bebida3");
+    bebida.classList.toggle("escolhido");
     const remover1 = document.querySelector(".bebida1");
     remover1.classList.remove("escolhido");
     const remover3 = document.querySelector(".bebida2");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function sobremesa1() {
-    const escolha = document.querySelector(".sobremesa1");
-    escolha.classList.toggle("escolhido");
+    sobremesa = document.querySelector(".sobremesa1");
+    sobremesa.classList.toggle("escolhido");
     const remover2 = document.querySelector(".sobremesa2");
     remover2.classList.remove("escolhido");
     const remover3 = document.querySelector(".sobremesa3");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function sobremesa2() {
-    const escolha = document.querySelector(".sobremesa2");
-    escolha.classList.toggle("escolhido");
+    sobremesa = document.querySelector(".sobremesa2");
+    sobremesa.classList.toggle("escolhido");
     const remover1 = document.querySelector(".sobremesa1");
     remover1.classList.remove("escolhido");
     const remover3 = document.querySelector(".sobremesa3");
     remover3.classList.remove("escolhido");
+    verificar();
 }
 
 function sobremesa3() {
-    const escolha = document.querySelector(".sobremesa3");
-    escolha.classList.toggle("escolhido");
+    sobremesa = document.querySelector(".sobremesa3");
+    sobremesa.classList.toggle("escolhido");
     const remover1 = document.querySelector(".sobremesa1");
     remover1.classList.remove("escolhido");
     const remover3 = document.querySelector(".sobremesa2");
     remover3.classList.remove("escolhido");
+    verificar();
+}
+
+function verificar() {
+    if (prato !== 0 && bebida !== 0 && sobremesa !== 0) {
+        const botao = document.querySelector(".caixa");
+        botao.classList.add("fechar-pedido");
+        botao.classList.remove("caixa");
+        botao.classList.add("fechar-pedido .fechar");
+    }
 }
