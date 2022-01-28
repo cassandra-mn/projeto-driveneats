a4f2f72391573219c818a2d832001b96aae1237f
cassandra-mn <arthenciac@gmail.com>
2022-01-28 00:53:58 -0300

    versão com itens marcados como selecionados

---
+++ script.js
@@ -0,0 +1,14 @@
+function prato1() {
+    const escolha = document.querySelector(".prato1");
+    escolha.classList.toggle("escolhido");
+}
+
+function prato2() {
+    const escolha = document.querySelector(".prato2");
+    escolha.classList.toggle("escolhido");
+}
+
+function prato3() {
+    const escolha = document.querySelector(".prato3");
+    escolha.classList.toggle("escolhido");
+}
