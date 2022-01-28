9112e10b4800be963e4b10c242c3d16721c5c58c
cassandra-mn <arthenciac@gmail.com>
2022-01-28 01:05:31 -0300

    versão com apenas um item selecionado

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function prato1() {
     const escolha = document.querySelector(".prato1");
     escolha.classList.toggle("escolhido");
+    const remover2 = document.querySelector(".prato2");
+    remover2.classList.remove("escolhido");
+    const remover3 = document.querySelector(".prato3");
+    remover3.classList.remove("escolhido");
 }
 
 function prato2() {
     const escolha = document.querySelector(".prato2");
     escolha.classList.toggle("escolhido");
+    const remover1 = document.querySelector(".prato1");
+    remover1.classList.remove("escolhido");
+    const remover3 = document.querySelector(".prato3");
+    remover3.classList.remove("escolhido");
 }
 
 function prato3() {
     const escolha = document.querySelector(".prato3");
     escolha.classList.toggle("escolhido");
+    const remover1 = document.querySelector(".prato1");
+    remover1.classList.remove("escolhido");
+    const remover3 = document.querySelector(".prato2");
+    remover3.classList.remove("escolhido");
 }
+
